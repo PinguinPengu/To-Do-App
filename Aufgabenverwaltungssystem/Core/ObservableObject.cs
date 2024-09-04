284a71caa71938b5d23e09ce8360ffee915d6770
PinguinPengu <der-henry@gmx.de>
2024-09-04 21:17:25 +0200

    First Gui Test Commit

--- Aufgabenverwaltungssystem/Core/ObservableObject.cs
+++ Aufgabenverwaltungssystem/Core/ObservableObject.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
 
 namespace Aufgabenverwaltungssystem.Core
 {
     internal class ObservableObject : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
     }
 }
